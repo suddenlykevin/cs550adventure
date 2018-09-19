5e45fa2fd226ddbd981c00ea9d56c4b651b4ad40
suddenlykevin <kxie19@choate.edu>
2018-09-18 21:50:31 -0400

    Practice
    In-class practice file

---
+++ adventurepractice.py
@@ -0,0 +1,19 @@
+#in-class guessing game practice
+#Kevin Xie 09/17/2018
+#Not the final project, but first attempt at a guessing game in class.
+
+import random
+
+def start():
+	guess = input("\nGuess how many humans I ate today!\n\n>>> ")
+	if int(guess) > answer:
+		print("\nDo I really look that fat? Lower.")
+		start()
+	elif int(guess) < answer:
+		print("\nI'll have you know I come from an affluent family. Higher.")
+		start()
+	elif int(guess) == answer: # could use "else"
+		print("\nRight on the money, sugar mama.")
+
+answer = random.randint(0,60)
+start()
